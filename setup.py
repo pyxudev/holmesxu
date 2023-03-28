27a799db69b5ca1d4d8c75052ea4ae0f3fc2378c
pyxudev <92786015+pyxudev@users.noreply.github.com>
2023-03-28 15:19:43 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='holmesxu',
+    author='Shuqi Xu',
+    author_email='xsq51py@gmail.com',
+    description='A simple tool to help on writing logical part while programming',
+    long_description='It provides tools to help your scripts be more simple and clean by containing some daily use grammers to functions to short your code.',
+    maintainer='Shuqi Xu',
+    maintainer_email='xsq51py@gmail.com',
+    license='GNU GENERAL PUBLIC LICENSE',
+    url='https://github.com/pyxudev',
+    version='0.1',
+    download_url='https://github.com/pyxudev/holmesxu.git',
+    python_requires='>=3.0',
+    install_requires='pip',
+    packages=['holmesxu/']
+)
